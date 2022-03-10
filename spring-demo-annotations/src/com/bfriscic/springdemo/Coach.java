35c395b73f7ec458aad27800f46b3239044783e4
Bernard Friščić <bernard.friscic@gmail.com>
2022-03-10 14:32:59 +0100

    Spring Configuration with Java Annotations - Inversion of Control

---
+++ spring-demo-annotations/src/com/bfriscic/springdemo/Coach.java
@@ -0,0 +1,7 @@
+package com.bfriscic.springdemo;
+
+public interface Coach {
+	
+	public String getDailyWorkout();
+
+}
---
+++ spring-demo-annotations/src/com/bfriscic/springdemo/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.bfriscic.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+}
